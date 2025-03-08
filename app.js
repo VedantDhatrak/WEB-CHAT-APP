fd7bb78fcdb25bc28a70a59ac2393cd67a2ddb4a
VedantDhatrak <vedantdhatrak13@gmail.com>
2025-03-08 12:33:22 +0530

    updated files -8

--- app.js
+++ app.js
@@ -8,7 +8,26 @@ const app = require('express')();
 const http = require('http').Server(app);
 
 const userRoute = require('./routes/userRoute');
+
+const User = require('./models/userModel')
+
 app.use('/', userRoute);
+
+const io = require('socket.io')(http);
+var usp = io.of('/user-namespace');
+usp.on('connection',async function(socket){
+    console.log('user connected');
+    var userId = socket.handshake.auth.token;
+    
+   await User.findByIdAndUpdate({ _id: userId}, { $set:{ is_online:'1'}})
+
+    socket.on('disconnect',async function(){
+        console.log('user disconnected')
+        var userId = socket.handshake.auth.token;
+    
+        await User.findByIdAndUpdate({ _id: userId}, { $set:{ is_online:'0'}})
+    });
+});
 
 http.listen(3000, function(){
     console.log('server is running')
--- controllers/userController.js
+++ controllers/userController.js
@@ -68,11 +68,14 @@ const logout = async(req,res) =>{
 }
 const loadDashboard = async(req,res) =>{
     try {
-        res.render('dashboard', {user: req.session.user});
+        var users = await User.find({ _id: {$nin:[req.session.user._id]}});
+        res.render('dashboard', {user: req.session.user, users:users});
     } catch (error) {
         console.log(error.message);
     }
 }
+
+
 
 module.exports = {
     registerLoad,
